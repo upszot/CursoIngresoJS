0c7966988c10a17cc4a6447c8be205658c5c25c3
upszot <upszot@gmail.com>
2016-11-21 19:56:34 -0300

    Add files via upload

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -7,20 +7,45 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
+
+
+function MiRandom(Inicial, Maximo)
+{// devuelve un Nro random entre el valor Inicial y el Maximo
+	return parseInt((Math.random() * Maximo) + Inicial );
+} // fin MiRandom
+
 var numeroSecreto; 
 var contadorIntentos;
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
+	numeroSecreto = MiRandom(1,100);
+    alert(numeroSecreto);
 	
+	contadorIntentos=0;
+	document.getElementById('intentos').value = 0;
 
 }
 
 function verificar()
-{
-	
-	
+{	
+	var Nro = document.getElementById('numero').value;
+	contadorIntentos++;
+	if(numeroSecreto == Nro)
+	{
+		alert("Usted es un ganador!!! y en solo X intentos");
+	}
+	else
+	{
+		document.getElementById('intentos').value = contadorIntentos;
+		if(Nro > numeroSecreto)
+		{
+			alert("Te pasaste");
+		}
+		else
+		{
+			alert("Te quedaste corto");	
+		}
+	}
 }
