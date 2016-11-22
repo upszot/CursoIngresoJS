0b5b1e156e2ead5db7ab9ed4d642e2f36cad3078
upszot <upszot@gmail.com>
2016-11-21 22:27:19 -0300

    Add files via upload

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -15,14 +15,74 @@ var contadorIntentos;
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
+	numeroSecreto = MiRandom(1,100);
+    alert(numeroSecreto);
 	
+	contadorIntentos=0;
+	document.getElementById('intentos').value = 0;
 
 }
 
 function verificar()
-{
+{	
+	var Nro = document.getElementById('numero').value;
 	
-	
+	contadorIntentos++;
+	document.getElementById('intentos').value = contadorIntentos;
 
+	if(numeroSecreto == Nro)
+	{
+		console.log("Acerto en el Intento: " + contadorIntentos);
+		alert(Mensaje(contadorIntentos));
+	}
+	else
+	{		
+		if(Nro > numeroSecreto)
+		{
+			alert("Te pasaste");
+		}
+		else
+		{
+			alert("Te quedaste corto");	
+		}
+	}
+}
+
+
+function MiRandom(Inicial, Maximo)
+{// devuelve un Nro random entre el valor Inicial y el Maximo
+	return parseInt((Math.random() * Maximo) + Inicial );
+} // fin MiRandom
+
+function Mensaje(CantIntentos)
+{
+	var sms;
+	switch (CantIntentos)
+	{
+		case 1:
+			sms="usted es un Psíquico";
+			break;
+		case 2:
+			sms="excelente percepción";
+			break;
+		case 3:
+			sms="Esto es suerte";
+			break;
+		case 4:
+			sms="Excelente técnica";
+			break;
+		case 5:
+			sms="usted está en la media";
+			break;
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+			sms="falta técnica";
+			break;							
+		default:
+			sms="afortunado en el amor!!";
+	}
+	return sms;
 }
