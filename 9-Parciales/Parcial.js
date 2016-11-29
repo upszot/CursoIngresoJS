79c9085ded33d516204190e2378c25c50b9eed2e
upszot <upszot@gmail.com>
2016-11-29 19:42:47 -0300

    Add files via upload

---
+++ 9-Parciales/Parcial.js
@@ -0,0 +1,208 @@
+
+
+function inicio()
+{	
+	var Nro;
+	Nro=prompt("ingrese Nro de ejercicio a probar: ");
+	switch(Nro)
+	{
+		case 1:
+			Nivel_1_1();
+			break;
+		case 2:
+			Nivel_1_2();
+			break;
+		case 3:
+			Nivel_1_3();
+			break;
+		case 4:
+			Nivel_2_4();
+			break;
+		case 5:
+			Nivel_2_5();
+			break;
+		case 6:
+			Nivel_3_6();
+			break;
+	}
+
+}//fin inicio
+
+
+
+// --------------------------- Nivel 1 -----------------------------
+function Nivel_1_1()
+{
+	var BaseCuadrado=parseInt(document.getElementById('base').value);
+	alert("El perimetro del cuadrado es: " + (BaseCuadrado * 4));
+}
+
+function Nivel_1_2()
+{
+	var importe;
+	var importe_iva;
+
+	importe=prompt("Ingrese el importe del producto: ");
+	importe_iva = importe + (importe*21/100 );
+	alert("El importe final es: " + importe_iva);
+}
+
+function Nivel_1_3()
+{
+	var largo= parseInt( document.getElementById('largo').value);
+	var ancho= parseInt( document.getElementById('ancho').value);
+	var perimetro;
+	var cantidad_vueltas=3;
+
+	perimetro=( (largo*2) + (ancho *2) );
+	alert("Se necesitaran " + ( perimetro * cantidad_vueltas) + " Metros de alambre");
+}
+
+/*
+Nivel 1 (entrada y salida de datos)
+1-realizar el algoritmo que lea la base por ID de un cuadrado e informar el perímetro del mismo por alert.
+2-realiazar el algoritmo que lea un importe por de un producto por prompt y muestre el importe final
+sumándole el IVA (21%).
+3- realizar el algoritmo que tome por ID dos datos, el largo y ancho de un terreno y luego que muestre
+cuantos metros de alambre se necesitan para colocarle 3 hilos de alambrado a su perímetro.
+*/
+
+// --------------------------- Nivel 2 -----------------------------
+function Nivel_2_4()
+{
+	var Numero1;
+	var Numero2;
+	var Resultado;
+
+	numero1=parseInt( prompt("Ingrese valor para el 1er Nro: ") );
+	numero2=parseInt( prompt("Ingrese valor para el 2do Nro: ") );
+
+	if(Numero1==Numero2)
+	{
+		Resultado= Numero1 * Numero2;
+	}
+	else
+	{
+		if(Numero1>Numero2)
+		{
+			Resultado= Numero1 - Numero2;
+		}
+		else
+		{
+			Resultado= Numero1 + Numero2;	
+		}
+	} //if(Numero1==Numero2)
+	document.write(Resultado);
+}
+
+function Nivel_2_5()
+{
+	var Dia;
+	Dia=prompt("Ingrese dia de la semana: ");
+	switch(Dia)
+	{
+		case "Sabado":
+		case "Domingo":
+			alert("es fin de semana");
+			break;
+		default:
+			alert("a trabajar !!!");
+	}
+}
+
+/*
+Nivel 2 (instrucción IF, SWITCH)
+4- (IF) realizar el algoritmo que pida dos números por prompt, si son iguales que los multiplique, si el
+primero es mayor al segundo que los reste y si no que los sume, mostrar el resultado por document.write.
+
+5- (SWITCH)realizar el algoritmo que pida un día de la semana por prompt, si es un “Sábado” o “Domingo”
+informar por alert “es fin de semana” de lo contrario informar por alert “a trabajar !!!” .
+*/
+
+// --------------------------- Nivel 3 -----------------------------
+
+function Nivel_3_6()
+{
+	var importe[24];
+	var dia;
+	var Importe_Mayor_Menor[2];
+
+	// posicion 0 para  el mayor y posicion 1 para  el menor
+	Importe_Mayor_Menor[0]=0;
+	Importe_Mayor_Menor[1]=0;
+
+	for (dia=0 ; dia<24 ; dia++)
+	{
+		importe[dia]=valida_impote();
+	}
+	
+	Importe_Mayor_Menor=BuscarDiaImporte(importe);
+
+	alert("El importe mayor corresponde al dia: " + Importe_Mayor_Menor[0] + " y es: $ " + importe[Importe_Mayor_Menor[0]]);
+
+	alert("El importe menor corresponde al dia: " + Importe_Mayor_Menor[1] + " y es: $ " + importe[Importe_Mayor_Menor[1]]);
+
+}
+
+function valida_impote()
+{
+	do
+		importe=prompt("Ingrese el importe de las ventas (mayor que 0): ");
+	while(importe < 0)
+
+	return importe;
+}
+
+function BuscarDiaImporte( importe[])
+{
+	var dia_del_importe_mayor=0;
+	var dia_del_importe_menor=0;
+	var aux[2];
+	//aux lo voy a usar en la posicion 0 para retornar el mayor y posicion 1 para retornar el menor
+	aux[0]=0;
+	aux[1]=0;
+	var i;
+
+	for(i=0 ; i<24 ; i++)
+	{
+		if( importe[i] > importe[dia_del_importe_mayor] )
+		{
+			dia_del_importe_mayor=i;
+		}
+
+		if(importe[i] < dia_del_importe_menor)
+		{
+			dia_del_importe_menor=i;
+		}
+	}
+	aux[0]=dia_del_importe_mayor;
+	aux[1]=dia_del_importe_menor;
+	return aux;
+}
+
+/*
+
+Nivel 3 (iteraciones – validaciones-máximos y mínimos)
+
+6- realizar el algoritmo que pida el importe de las ventas (validar que sea mayor a 0,”cero”) de los 24 días
+hábiles del mes por prompt (una por día), e informar cual fue el mayor importe y cuál fue el menor
+importe de venta.
+
+7- realizar el algoritmo que permita el ingreso por prompt de las notas (validar entre 0 y 10) y el sexo
+(validar el sexo “f” o “m”) de 100 alumnos, informar por alert:
+a) el promedio de las notas totales.
+b) la nota más baja.
+c) la cantidad de varones que su nota haya sido mayor o igual a 6.
+*/
+
+// --------------------------- Nivel 4 -----------------------------
+
+/*
+Nivel 4
+8- realizar el algoritmo que permita ingresar números positivos (validar que sea positivo) hasta que el
+usuario quiera e informar al terminar el ingreso por document.write:
+a) la cantidad de números pares.
+b) el promedio de todos los números ingresados.
+c) la suma de todos los números.
+d) El número máximo y el mínimo
+*/
